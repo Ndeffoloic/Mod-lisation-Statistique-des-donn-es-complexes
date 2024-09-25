2281f305abb50ddc5aac15b856ca146125c8cdae
nembotnd1u <nembotnd1u@etu.univ-lorraine.fr>
2024-09-25 11:28:50 +0200

    je n'utilise plus tkinter

--- Cours 2/tp2.py
+++ Cours 2/tp2.py
@@ -1,11 +1,12 @@
-from tkinter import Tk
-from tkinter.filedialog import askopenfilename
+import os
 
 import pandas as pd
 
-# Ouvrir une boîte de dialogue pour choisir le fichier
-Tk().withdraw()  # Empêche l'apparition de la fenêtre principale de Tkinter
-file_path = askopenfilename()
+# Nom du fichier à ouvrir (assurez-vous de remplacer 'nom_du_fichier.txt' par le nom réel de votre fichier)
+file_name = 'poumon.txt'
+
+# Obtenir le chemin complet du fichier
+file_path = os.path.join(os.path.dirname(__file__), file_name)
 
 # Lire le fichier dans un DataFrame avec les bons séparateurs
 data = pd.read_csv(file_path, sep='\t', decimal='.')
